4965b2482e3d5d08bd7fe64007649ae30cd8ab1d
ponugotikarthik <karthikponugoti1@gmail.com>
2022-11-20 22:23:54 +0530

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppComponent } from './app.component';
+import { AirQualityComponent } from './air-quality/air-quality.component';
+import { TemperatureComponent } from './temperature/temperature.component';
+import { AudioComponent } from './audio/audio.component';
+import { DisplaysComponent } from './displays/displays.component';
+import { LightingComponent } from './lighting/lighting.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    AirQualityComponent,
+    TemperatureComponent,
+    AudioComponent,
+    DisplaysComponent,
+    LightingComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
